d8479c01c7ed0d877ade0f4e7e954a694f2b0799
shogg <lars.heyden@gmail.com>
2022-05-29 00:05:44 +0200

    shader as frame counter access

--- assets/shaders/custom_material.wgsl
+++ assets/shaders/custom_material.wgsl
@@ -1,11 +1,19 @@
+struct VertexOutput {
+    [[builtin(position)]] clip_position: vec4<f32>;
+    [[location(0)]] world_position: vec4<f32>;
+    [[location(1)]] world_normal: vec3<f32>;
+    [[location(2)]] uv: vec2<f32>;
+};
+
 struct ShaderMaterial {
     color: vec4<f32>;
+    frame: u32;
 };
 
 [[group(1), binding(0)]]
 var<uniform> material: ShaderMaterial;
 
 [[stage(fragment)]]
-fn fragment() -> [[location(0)]] vec4<f32> {
-    return material.color.xyzw * .7;
+fn fragment(input: VertexOutput) -> [[location(0)]] vec4<f32> {
+    return material.color * 0.1 * input.uv.xyxy * f32(material.frame / u32(1.25) % u32(30));
 }
--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ use bevy::{
 	prelude::*,
 	reflect::TypeUuid,
 	render::{
-        render_asset::{PrepareAssetError, RenderAsset},
+        render_asset::{PrepareAssetError, RenderAsset, RenderAssets},
         render_resource::{
             std140::{AsStd140, Std140},
             *,
         },
-        renderer::RenderDevice,
+        renderer::{RenderDevice, RenderQueue},
+        RenderStage, RenderApp,
     },
 	sprite::*, asset::Assets,
 };
 
 fn main() {
-    App::new()
-        .insert_resource(AssetServerSettings {
+    let mut app = App::new();
+
+    app.insert_resource(AssetServerSettings {
             watch_for_changes: true,
             asset_folder: "assets".to_string(),
             ..default()
@@ -24,8 +26,13 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(Material2dPlugin::<ShaderMaterial>::default())
         .add_system(log_asset_events)
-        .add_startup_system(setup)
-        .run();
+        .add_startup_system(setup);
+
+    app.sub_app_mut(RenderApp)
+    .add_system_to_stage(RenderStage::Extract, extract_material)
+    .add_system_to_stage(RenderStage::Prepare, prepare_material);
+
+    app.run();
 }
 
 pub fn setup(
@@ -41,7 +48,8 @@ pub fn setup(
         mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
         transform: Transform::default().with_scale(Vec3::splat(256.)),
         material: materials.add(ShaderMaterial {
-            color: Color::GREEN,
+            color: Vec4::from_slice(&Color::GOLD.as_linear_rgba_f32()),
+            frame: 0,
         }),
         ..Default::default()
     });
@@ -51,24 +59,48 @@ pub fn log_asset_events(
     mut ev_reader: EventReader<AssetEvent<Shader>>,
 ) {
     for ev in ev_reader.iter() {
-        println!("{:?}", ev)
+        println!("{:?}", ev);
     }
 }
 
-#[derive(Debug, Clone, TypeUuid)]
+pub fn extract_material(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ShaderMaterial>>,
+) {
+    for m in materials.iter_mut() {
+        m.1.frame += 1;
+        commands.insert_resource(m.1.clone());
+    }
+}
+
+pub fn prepare_material(
+    mut material: ResMut<ShaderMaterial>,
+    mut material_gpu_assets: ResMut<RenderAssets<ShaderMaterial>>,
+    render_queue: Res<RenderQueue>,
+) {
+    for material_gpu in material_gpu_assets.values_mut() {
+        material.color = Vec4::from_slice(&Color::GOLD.as_linear_rgba_f32());
+        material.frame += 1;
+        render_queue.write_buffer(&material_gpu.buffer, 0, material.as_std140().as_bytes());
+    }
+    println!("Frame {:?}", material.frame);
+}
+
+#[derive(Debug, Clone, TypeUuid, AsStd140)]
 #[uuid = "4ee9c363-1124-4113-890e-199d81b00281"]
 pub struct ShaderMaterial {
-	pub color: Color
+	pub color: Vec4,
+    pub frame: u32,
 }
-pub struct GpuShaderMaterial {
-    pub _buffer: Buffer,
+pub struct ShaderMaterialGpu {
+    pub buffer: Buffer,
     pub bind_group: BindGroup,
 }
 
 impl RenderAsset for ShaderMaterial {
 
     type ExtractedAsset = ShaderMaterial;
-    type PreparedAsset = GpuShaderMaterial;
+    type PreparedAsset = ShaderMaterialGpu;
     type Param = (SRes<RenderDevice>, SRes<Material2dPipeline<Self>>);
 
     fn extract_asset(&self) -> Self::ExtractedAsset {
@@ -79,23 +111,25 @@ impl RenderAsset for ShaderMaterial {
         extracted_asset: Self::ExtractedAsset,
         (render_device, material_pipeline): &mut SystemParamItem<Self::Param>,
     ) -> Result<Self::PreparedAsset, PrepareAssetError<Self::ExtractedAsset>> {
-        let color = Vec4::from_slice(&extracted_asset.color.as_linear_rgba_f32());
+
         let buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
-            contents: color.as_std140().as_bytes(),
+            contents: extracted_asset.as_std140().as_bytes(),
             label: None,
             usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
         });
         let bind_group = render_device.create_bind_group(&BindGroupDescriptor {
-            entries: &[BindGroupEntry {
-                binding: 0,
-                resource: buffer.as_entire_binding(),
-            }],
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: buffer.as_entire_binding(),
+                },
+            ],
             label: None,
             layout: &material_pipeline.material2d_layout,
         });
 
-        Ok(GpuShaderMaterial {
-            _buffer: buffer,
+        Ok(ShaderMaterialGpu {
+            buffer,
             bind_group,
         })
     }
@@ -113,16 +147,18 @@ impl Material2d for ShaderMaterial {
 
     fn bind_group_layout(render_device: &RenderDevice) -> BindGroupLayout {
         render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
-            entries: &[BindGroupLayoutEntry {
-                binding: 0,
-                visibility: ShaderStages::FRAGMENT,
-                ty: BindingType::Buffer {
-                    ty: BufferBindingType::Uniform,
-                    has_dynamic_offset: false,
-                    min_binding_size: BufferSize::new(Vec4::std140_size_static() as u64),
+            entries: &[
+                BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: BufferSize::new(Vec4::std140_size_static() as u64 + 16),
+                    },
+                    count: None,
                 },
-                count: None,
-            }],
+            ],
             label: None,
         })
     }
